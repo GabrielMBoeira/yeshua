d7f86580c796ccf65b88346efca4e4deaf447d58
GabrielMBoeira <gboeira777@gmail.com>
2023-08-05 15:27:59 -0300

    comments

--- db/connect.php
+++ db/connect.php
@@ -7,46 +7,46 @@ class Connection
 
         var_dump(getenv('DATABASE'));
         die;
-        
-        $envFilePath = '.env';
 
-        if ($mainPath) {
-            $envFileContent = file_get_contents($envFilePath);
-        } else {
-            $envFileContent = file_get_contents('../' . $envFilePath);
-        }
+        // $envFilePath = '.env';
 
-        if ($envFileContent === false) {
-            die("Não foi possível ler o arquivo .env");
-        }
+        // if ($mainPath) {
+        //     $envFileContent = file_get_contents($envFilePath);
+        // } else {
+        //     $envFileContent = file_get_contents('../' . $envFilePath);
+        // }
 
-        $envVariables = preg_split('/\r\n|\r|\n/', $envFileContent);
+        // if ($envFileContent === false) {
+        //     die("Não foi possível ler o arquivo .env");
+        // }
 
-        $envData = [];
-        foreach ($envVariables as $envVariable) {
-            $parts = explode('=', $envVariable, 2);
-            if (count($parts) === 2) {
-                $key = trim($parts[0]);
-                $value = trim($parts[1]);
-                $envData[$key] = $value;
-            }
-        }
+        // $envVariables = preg_split('/\r\n|\r|\n/', $envFileContent);
 
-        $banco = $envData['DATABASE'];
-        $servidor = $envData['DATA_BASE_SERVER'];
-        $usuario = $envData['DATA_BASE_USER'];
-        $senha = $envData['DATA_BASE_PASSWORD'];
-        $port = $envData['DATA_BASE_PORT'];
+        // $envData = [];
+        // foreach ($envVariables as $envVariable) {
+        //     $parts = explode('=', $envVariable, 2);
+        //     if (count($parts) === 2) {
+        //         $key = trim($parts[0]);
+        //         $value = trim($parts[1]);
+        //         $envData[$key] = $value;
+        //     }
+        // }
 
-        var_dump($servidor, $usuario, $senha, $banco, $port);
-        die;
+        // $banco = $envData['DATABASE'];
+        // $servidor = $envData['DATA_BASE_SERVER'];
+        // $usuario = $envData['DATA_BASE_USER'];
+        // $senha = $envData['DATA_BASE_PASSWORD'];
+        // $port = $envData['DATA_BASE_PORT'];
 
-        $conn = new mysqli($servidor, $usuario, $senha, $banco, $port);
+        // var_dump($servidor, $usuario, $senha, $banco, $port);
+        // die;
 
-        if ($conn->connect_error) {
-            die('Erro: ' . $conn->connect_error);
-        }
+        // $conn = new mysqli($servidor, $usuario, $senha, $banco, $port);
 
-        return $conn;
+        // if ($conn->connect_error) {
+        //     die('Erro: ' . $conn->connect_error);
+        // }
+
+        // return $conn;
     }
 }
